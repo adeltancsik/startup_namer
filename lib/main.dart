11baff528ca474d3dc230e620bce2f6d7641f429
adeltancsik <tancsikadel@gmail.com>
2020-06-17 12:00:30 +0200

    Adding comments

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,14 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
   return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
+      itemBuilder: (context, i) /* the iterator begins at 0 and increments each time the function is called */ { 
+        // called once per suggested word pairing, and places each suggestion into a ListTile row
+        if (i.isOdd) return Divider(); // for every odd iteration a divider is added instead of a ListTile/list item
+        // add a one-pixel-high divider widget before each row in the ListView (for odd rows)
 
-        final index = i ~/ 2; /*3*/
+        final index = i ~/ 2; // divides i by 2 and returns an integer result => calculates the actual number of word pairings in the ListView
         if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          _suggestions.addAll(generateWordPairs().take(10)); // if the end of the available word pairings is reached, then generate 10 more and add them to the suggestions list => infinite list
         }
         return _buildRow(_suggestions[index]);
       });
