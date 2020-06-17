fbd99ce764442e32af960576794b89391aad0077
adeltancsik <tancsikadel@gmail.com>
2020-06-17 18:42:12 +0200

    Ineractivity, routing, changing the theme color

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',            
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.redAccent[100],
+      ),
       home: RandomWords(),
     );
   }
@@ -19,52 +22,89 @@ class RandomWordsState extends State<RandomWords> {
   final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
-  return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) /* the iterator begins at 0 and increments each time the function is called */ { 
-        // called once per suggested word pairing, and places each suggestion into a ListTile row
-        if (i.isOdd) return Divider(); // for every odd iteration a divider is added instead of a ListTile/list item
-        // add a one-pixel-high divider widget before each row in the ListView (for odd rows)
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context,
+            i) /* the iterator begins at 0 and increments each time the function is called */ {
+          // called once per suggested word pairing, and places each suggestion into a ListTile row
+          if (i.isOdd)
+            return Divider(); // for every odd iteration a divider is added instead of a ListTile/list item
+          // add a one-pixel-high divider widget before each row in the ListView (for odd rows)
 
-        final index = i ~/ 2; // divides i by 2 and returns an integer result => calculates the actual number of word pairings in the ListView
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); // if the end of the available word pairings is reached, then generate 10 more and add them to the suggestions list => infinite list
-        }
-        return _buildRow(_suggestions[index]);
-      });
-}
+          final index = i ~/
+              2; // divides i by 2 and returns an integer result => calculates the actual number of word pairings in the ListView
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(
+                10)); // if the end of the available word pairings is reached, then generate 10 more and add them to the suggestions list => infinite list
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 
-Widget _buildRow(WordPair pair) {
-  final bool alreadySaved = _saved.contains(pair);
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-    trailing: Icon(
-      alreadySaved ? Icons.favorite : Icons.favorite_border,
-      color: alreadySaved ? Colors.red : null,
-    ),
-    onTap: () {
-      setState(() {
-        if (alreadySaved) {
-          _saved.remove(pair);
-        } else { 
-          _saved.add(pair); 
-        } 
-      });
-    }, // function on tap
-  );
-}
-  
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }, // function on tap
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-  );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
